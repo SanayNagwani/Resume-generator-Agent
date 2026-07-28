c7c7ffee136f1022e1b4341df38d7518936d81ba
SanayNagwani <nagwanisanay@gmail.com>
2026-07-28 16:39:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,22 +165,19 @@ job name, job desc, salary,
 apply link and OUTPUT must be In HTML no markdowns"""
 
 
-if st.button("Generate Resume"):
-  with st.spinner("Agent creating Resume..."):
-   
-    responce = agent.invoke({'messages':[{'role':'user',"content":final_query}]})
-    code = responce['messages'][-1].content[-1]['text']
+if st.button('generate resume'):
+  with st.spinner("Running Agent"):
 
+    response = agent.invoke({'messages': [{'role':'user','content':query}]})
+    code=response['messages'][-1].content[-1]['text']
+          
+    if FILE is not None:
+        with open(save_path, "rb") as img_file:
+            b64_image = base64.b64encode(img_file.read()).decode()
+        data_uri = f"data:image/jpeg;base64,{b64_image}"
+        code = code.replace("PROFILE_IMAGE_PLACEHOLDER", data_uri)
 
-     if FILE is not None:
-        with open (save_path, "rb") as img_file:
-          b64_image = base64.b64encode(img_file.read()).decode()
-        data_url = f"data:image/jpeg;base64,{b64_image}"
-        code = code.replace("PROFILE_IMAGE_PLACEHOLDER",data_url)
-    
-
-    st.html(code, width="stretch", unsafe_allow_javascript=True)
-
+    st.html(code , width="stretch" , unsafe_allow_javascript=True)
 
 #=========================== Apply live jobs =============================
     st.divider()
